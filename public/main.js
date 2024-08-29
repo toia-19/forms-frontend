d0e818fa0c7d99ad917ab3575e56ee1ef887e18b
toia-19 <vickingiediaz19@gmail.com>
2024-08-29 20:04:46 -0300

    Subiendo formulario inicial

--- public/main.js
+++ public/main.js
@@ -1,48 +1,9 @@
-// Declaración de constantes
-const url = "../assets/docs/students.json";
-const overlay = document.querySelector('.overlay');
-const input = document.getElementById('input');
-const names = document.querySelector('.list');
+// Variables para atributos de usuario
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+const contrasena = document.getElementById('contrasena');
+const color_fav = document.getElementById('color_fav');
+const mensaje = document.getElementById('mensaje');
 
-// Arreglo de estudiantes
-let studentsArrayObj = [];
-let studentsNames = [];
-
-// Función para obtener estudiantes
-async function getStudents() {
-    overlay.classList.add('active');
-
-    try {
-        const response = await fetch(url);
-        studentsArrayObj = await response.json();
-        
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-getStudents()
-.then(() => {
-    console.log(studentsArrayObj);
-    
-    // Recorremos arreglo y obtenemos los nombres que contiene
-    for (let i = 0; i < studentsArrayObj.length; i++) {
-        const e = studentsArrayObj[i];
-
-        // Se "pushea" a la colección de "studentsNames"
-        studentsNames.push(e.nombre);
-    }
-})
-.then(() => {
-    // Ordenamos el arreglo con los nombres
-    let sortedNames = studentsNames.sort();
-
-    setTimeout(() => {
-        overlay.classList.remove('active');
-        
-        //names.outerHTML.println(sortedNames[e]);
-        //names.innerHTML = `<li>${sortedNames.length}</li>`
-        //names.innerHTML = sortedNames;
-
-    },500)
-})
+// Botón para enviar información
+const boton = document.getElementById('btn-enviar');
